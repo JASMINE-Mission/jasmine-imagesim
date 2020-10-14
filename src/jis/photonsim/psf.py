fd5930f4a630633d691f4f8b62ff5ceb37f8be6a
T. Kamizuka <kamizuka@ioa.s.u-tokyo.ac.jp>
2020-10-14 16:03:49 +0900

    Bug fix(!!) and adding docstring (TK).

--- src/jis/photonsim/psf.py
+++ src/jis/photonsim/psf.py
@@ -2,50 +2,103 @@ import math
 import numpy as np
 import pyfftw
 
-def calc_psf(wfe,wN,k,WL,NP,Ntot,Stel,adata,M,aN):
+def calc_psf(wfe, wN, k, WL, NP, Ntot, Stel, adata, M, aN):
+    """
+    Summary:
+        This function calculates the psf in e-/sec/pix
+        based on the wavefront error (wfe),
+        the spectral information (NP), and
+        the aperture mask data (adata)..
+
+    Args:
+        wfe   (ndarray): Wavefront error data (um?).
+        wN    (int)    : Number of pixels on a side of the wfe data.
+        k     (int)    : Number of wavelength data points.
+        WL    (ndarray): Wavelength data (um?).
+        NP    (ndarray): Number of photons data?
+        Ntot  (float)  : Total number of photons (electrons)?
+        Stel  (float)  : Total area of the telescope aperture.
+        adata (ndarray): Aperture mask data.
+        M              : Control parameter M?.
+        aN    (int)    : Number of pixels of the aperture mask data.
+
+    Returns:
+        image (nadarray): 520 x 520 pixel array of the psf in e-/s/pix.
+
+    Example:
+        import json
+        from jis.photonsim import extract_json
+        from jis.photonsim import readfits
+
+        # Reading source information.
+        sp = json.load(open("source.json"))
+        k,WL,NP,Ntot = extract_json.extsp(sp)
+
+        # Reading aperture pattern.
+        Stel,adata,aN,ahdr = readfits.read_aperture_mask("aperture.fits")
+
+        # Reading wavefront error pattern.
+        wfe,wN,whdr=readfits.read_wfe_map("wfe.fits")
+
+        # Reading control parameters.
+        cp = json.load(open("ctrl_param.json"))
+        M = cp['M']['val']
+
+        # Getting psf pattern.
+        image = psf.calc_psf(wfe,wN,k,WL,NP,Ntot,Stel,adata,M,aN)
+   
+
+    """
+        
     wfer = np.nan_to_num( wfe )
     wfec = np.empty( (wN,wN),dtype='complex128')
+
     # 波長を k 個の点で規定している。
     # 口径 D で、計算領域の大きさを N とするとき、
     # フーリエ変換で得られる PSF は D/N x lambda/D=lambda/N の角度スケールになる。 
-    # 異なるいくつかの波長 WL0,WL1,,,WLn で生成したPSFのセルスケールを
+    # 異なるいくつかの波長 WL0, WL1, ..., WLn で生成したPSFのセルスケールを
     # 合わせようとおもったら、計算領域を波長に比例させて
     # N0 = M WL0 , N1 = M WL1 ,,, Nn = M WLn 
     # と選べばよい。
-    N0 = 520
+
+    N0    = 520
     image = np.zeros( (N0,N0) ,dtype ='float' )
+
     # WL として、1.1, 1.2, ,,, 1.6 としているとき、
     # 1.1-1.2 の範囲のフォトンを 波長 1.15 で代表させて加え、
     # 1.2-1.3, ,,, 1.5-1.6 を加える、としていこう。 
     for i in range(k-1):
         WLm = (WL[i] + WL[i+1])/2 
         N = int(WLm*M)
+
         # initialize data for FFT
         data = pyfftw.zeros_aligned((N,N),dtype='complex128')
+
         #  put the mask on the data assuming N > aN
         i1 = int(N/2-aN/2)
         i2 = int(N/2+aN/2)
         data.real[i1:i2,i1:i2] = adata[:,:]
         
-    # ここで、 WFE map を波長に反比例させて大きさをかえてから、
-    # 位相成分として複素数化して data に掛ける
-    wfec.real = np.cos(wfer/WLm)
-    wfec.imag = np.sin(wfer/WLm)
-    i3 = int(N/2-wN/2)
-    i4 = int(N/2+wN/2)
-    data[i3:i4,i3:i4] = data[i3:i4,i3:i4] * wfec
+        # ここで、 WFE map を波長に反比例させて大きさをかえてから、
+        # 位相成分として複素数化して data に掛ける
+        wfec.real = np.cos(wfer/WLm)
+        wfec.imag = np.sin(wfer/WLm)
+        i3 = int(N/2-wN/2)
+        i4 = int(N/2+wN/2)
+        data[i3:i4,i3:i4] = data[i3:i4,i3:i4] * wfec
     
-    # FFT
-    ft = pyfftw.interfaces.numpy_fft.fft2(data)
-    fts = np.fft.fftshift(ft)
+        # FFT
+        ft  = pyfftw.interfaces.numpy_fft.fft2(data)
+        fts = np.fft.fftshift(ft)
     
-    # 結果を入射光子数(電子数)の重みを付けて加算する。
-    i1 = int(N/2-N0/2)
-    i2 = int(N/2+N0/2)
-    NPm = (NP[i] + NP[i+1])/2
-    image = image + NPm*(fts.real[i1:i2,i1:i2]**2 + fts.imag[i1:i2,i1:i2]**2)
+        # 結果を入射光子数(電子数)の重みを付けて加算する。
+        i1 = int(N/2-N0/2)
+        i2 = int(N/2+N0/2)
+        NPm   = (NP[i] + NP[i+1])/2
+        image = image + NPm*(fts.real[i1:i2,i1:i2]**2 + fts.imag[i1:i2,i1:i2]**2)
     
     # 最後に規格化しておく、値は1秒あたりのelectron数になるように
-    s = np.sum(image)
+    s     = np.sum(image)
     image = image/s * Ntot * Stel
+
     return image
