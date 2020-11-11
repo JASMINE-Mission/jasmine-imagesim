83dddf4cf80dff1e39c3b6981485e10c2cb186be
T. Kamizuka <kamizuka@ioa.s.u-tokyo.ac.jp>
2020-11-12 07:24:34 +0900

    Making detector object in mkimage.py (TK).

--- bin/mkimage.py
+++ bin/mkimage.py
@@ -4,16 +4,19 @@
   Make an image
 
   usage:
-    mkimage.py [-h|--help] --starplate star_plate.csv 
+    mkimage.py [-h|--help] --starplate star_plate.csv --det det.json 
 
  options:
    --help                     show this help message and exit
    --starplate star_plate.csv csv file containing star info (plate_id, star_id, xpix, ypix, l, b)
+   --det det.json             json file containing detector related parameters.
+
 """
 
 from docopt import docopt
 import numpy as np
 import astropy.io.ascii as asc
+from jis.photonsim.extract_json import mkDet
 
 
 # Command line interface
@@ -22,12 +25,13 @@ if __name__ == '__main__':
 
     # Get parameters from command line
     filename_starplate = args['--starplate']
+    filename_detjson   = args['--det']
 
     # Loading parameters.
     table_starplate = asc.read(filename_starplate)
+    detector = mkDet(filename_detjson)
 
     # Selecting the data for the first plate.
     pos = np.where(table_starplate['plate_id']==0)
     table_starplate = table_starplate[pos]
 
-    print(table_starplate)
