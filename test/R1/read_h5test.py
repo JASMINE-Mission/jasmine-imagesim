8c91c05d015da907e1205fb1d31f85d999c59158
Hajime Kawahara <divrot@gmail.com>
2020-12-16 11:13:10 +0900

    test completed

--- test/R1/read_h5test.py
+++ test/R1/read_h5test.py
@@ -6,8 +6,8 @@ with h5py.File("image.h5","r") as f:
 print(np.shape(cube))
 plt.imshow(cube[0:200,0:200,0])
 plt.show()
-s=50
-e=100
+s=0
+e=50
 lc=np.sum(cube[s:e,s:e,:],axis=(0,1))
 norm=np.median(lc)
 lc=lc/norm
