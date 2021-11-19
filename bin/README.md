b8e8de4492950303513f6adc4442c6bc24d2961b
Hajime Kawahara <divrot@gmail.com>
2021-11-19 20:25:41 +0900

    README

---
+++ bin/README.md
@@ -0,0 +1,13 @@
+# binary directory
+
+- mkimage.py primary code example for astrometry/photometry
+- mkimage_nosub.py code example whose output is a pair of the raw frames (no subtraction)
+
+## sh
+
+- test_light.sh astrometry mode
+- test_transit.sh transit obs
+
+## history
+
+other test code were moved to test/bintest directory (Nov 19th 2021)
--- bin/test_transit.sh
+++ bin/test_transit.sh
@@ -1,4 +1,3 @@
 echo "Light version of test.sh for debugging"
 
-#python mkimage_transit.py --pd ../params/templates/ --starplate star_plate.csv --det det.json --tel tel.json --ace ace_001.json --ctl ctlp.json --var variability.json --format platefits --overwrite --dft drift.json
-python mkimage_transit.py --pd ../params/templates/ --starplate star_plate.csv --det det.json --tel tel.json --ace ace_001.json --ctl ctl.json --format platefits --overwrite --var variability.json
+python mkimage.py --pd ../params/templates/ --starplate star_plate.csv --det det.json --tel tel.json --ace ace_001.json --ctl ctl_planet.json --format platefits --overwrite --var variability.json --dft drift.json
