502b7cf7363b95e155d0d89d4906f034ad346b30
Hajime Kawahara <divrot@gmail.com>
2021-11-24 10:17:21 +0900

    added csv to new params directory

--- bin/test_transit.sh
+++ bin/test_transit.sh
@@ -1,3 +1,3 @@
 echo "Light version of test.sh for debugging"
 
-python mkimage.py --pd ../params/templates/ --starplate star_plate.csv --det det.json --tel tel.json --ace ace_001.json --ctl ctl_planet.json --format platefits --overwrite --var variability.json --dft drift.json
+python mkimage.py --pd ../params/planet/ --starplate star_plate.csv --det det.json --tel tel.json --ace ace_001.json --ctl ctl.json --format platefits --overwrite --var variability.json --dft drift.json
