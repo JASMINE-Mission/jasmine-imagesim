de441a6c1c59dcc65aec1b5a7bec3d7aa6af80a3
Hajime Kawahara <divrot@gmail.com>
2021-11-24 11:11:42 +0900

    reduced dtace for exoflat to 3kHz

--- bin/test_exoflat.sh
+++ bin/test_exoflat.sh
@@ -1,3 +1,3 @@
 echo "Light version of test.sh for debugging"
 
-python mkimage.py --pd ../params/templates/ --starplate star_plate.csv --det det_exoflat.json --tel tel.json --ace ace_001.json --ctl ctl_exoflat.json --format platefits --overwrite --var variability.json --dft drift.json
+python mkimage.py --pd ../params/exoflat/ --starplate star_plate.csv --det det.json --tel tel.json --ace ace_001.json --ctl ctl.json --format platefits --overwrite --var variability.json --dft drift.json
