94c349cdce002c739dc9c335fc17d5b6e61a9e7c
Hajime Kawahara <divrot@gmail.com>
2021-11-30 11:56:10 +0900

    removed test_light_noace.sh

--- bin/test_light_noace.sh
+++
@@ -1,2 +0,0 @@
-echo "Light version for debugging noace"
-python mkimage.py --pd ../params/templates/ --starplate star_plate.csv --det det.json --tel tel.json --ace ace_001.json --ctl ctl.json --format platefits --overwrite --var variability.json --noace
